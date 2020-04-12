a4fb8a47a3cf17b3a0e09de0788a010914965ea8
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2020-04-12 13:00:26 -0400

    enable dark mode as default

---
+++ assets/main.js
@@ -0,0 +1,133 @@
+!(function() {
+  document.addEventListener('DOMContentLoaded', init);
+
+  const color = '#a7a7a7';
+
+  function init() {
+    let theme = 'dark';
+    if (localStorage) {
+      theme = localStorage.getItem('theme') || theme;
+    }
+
+    document.body.classList.add(theme);
+
+    const particleConfig = {
+      particles: {
+        number: {
+          value: 80,
+          density: {
+            enable: true,
+            value_area: 800
+          }
+        },
+        color: {
+          value: color
+        },
+        shape: {
+          type: 'circle',
+          stroke: {
+            width: 0,
+            color: color
+          },
+          polygon: {
+            nb_sides: 5
+          }
+        },
+        opacity: {
+          value: 0.5,
+          random: false,
+          anim: {
+            enable: false,
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false
+          }
+        },
+        size: {
+          value: 3,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 40,
+            size_min: 0.1,
+            sync: false
+          }
+        },
+        line_linked: {
+          enable: true,
+          distance: 110.48066982851817,
+          color: color,
+          opacity: 0.4,
+          width: 1
+        },
+        move: {
+          enable: true,
+          speed: 6,
+          direction: 'none',
+          random: false,
+          straight: false,
+          out_mode: 'out',
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200
+          }
+        }
+      },
+      interactivity: {
+        detect_on: 'canvas',
+        events: {
+          onhover: {
+            enable: false,
+            mode: 'repulse'
+          },
+          onclick: {
+            enable: true,
+            mode: 'push'
+          },
+          resize: true
+        },
+        modes: {
+          grab: {
+            distance: 400,
+            line_linked: {
+              opacity: 1
+            }
+          },
+          bubble: {
+            distance: 400,
+            size: 40,
+            duration: 2,
+            opacity: 8,
+            speed: 3
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4
+          },
+          push: {
+            particles_nb: 4
+          },
+          remove: {
+            particles_nb: 2
+          }
+        }
+      },
+      retina_detect: true
+    };
+
+    document.getElementById('switch').addEventListener('click', function() {
+      if (localStorage) {
+        const currentTheme = localStorage.getItem('theme');
+        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+        localStorage.setItem('theme', newTheme);
+        document.body.classList.remove(currentTheme);
+        document.body.classList.add(newTheme);
+      }
+    });
+
+    particlesJS('particles-js', particleConfig);
+  }
+
+})();
